b7f97d69fc3f4ed9fa0dc6a873299c8f514cc246
Ekansh Saxena <ekansh.a.saxena@capgemini.com>
2022-02-18 10:53:26 +0200

    Add markdown editor on detail side

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,3 +1,32 @@
+import * as React from "react";
+import ReactMde from "react-mde";
+import * as Showdown from "showdown";
+import "react-mde/lib/styles/css/react-mde-all.css";
+
+const converter = new Showdown.Converter({
+  tables: true,
+  simplifiedAutoLink: true,
+  strikethrough: true,
+  tasklists: true,
+});
+
 export default function Detail() {
-  return <h1>Detail</h1>;
+  const [value, setValue] = React.useState("**Hello world!!!**");
+  const [selectedTab, setSelectedTab] = React.useState("write");
+  const toggleTab = () => {
+    setSelectedTab(selectedTab === "write" ? "preview" : "write");
+  };
+  return (
+    <div className="container">
+      <ReactMde
+        minEditorHeight={78}
+        heightUnits="vh"
+        value={value}
+        onChange={setValue}
+        selectedTab={selectedTab}
+        onTabChange={toggleTab}
+        generateMarkdownPreview={(markdown) => Promise.resolve(converter.makeHtml(markdown))}
+      />
+    </div>
+  );
 }
