35d613cace699e7a43f7be35238ffeb2feafa285
Ekansh Saxena <ekansh005@gmail.com>
2022-02-22 20:04:21 +0200

    Add supabase

--- src/App.js
+++ src/App.js
@@ -5,10 +5,21 @@ import removeMarkdown from "remove-markdown";
 import List from "./components/List";
 import Detail from "./components/Detail";
 import Header from "./components/Header";
+import Login from "./components/Login";
+import { supabase } from "./supabaseClient";
 
 function App() {
   const [notes, setNotes] = React.useState(JSON.parse(localStorage.getItem("notes") || "[]"));
   const [selectedNoteId, setSelectedNoteId] = React.useState((notes[0] && notes[0].id) || null);
+  const [session, setSession] = React.useState(null);
+
+  React.useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
 
   React.useEffect(() => {
     localStorage.setItem("notes", JSON.stringify(notes));
@@ -59,28 +70,32 @@ function App() {
 
   return (
     <>
-      <Header />
-      <Split
-        className="split"
-        sizes={[25, 75]}
-        minSize={100}
-        expandToMin={false}
-        gutterSize={10}
-        gutterAlign="center"
-        snapOffset={30}
-        dragInterval={1}
-        direction="horizontal"
-        cursor="col-resize"
-      >
-        <List
-          selectedNoteId={selectedNoteId}
-          addNote={addNote}
-          deleteNote={deleteNote}
-          selectNote={selectNote}
-          notes={notes}
-        />
-        <Detail selectedNote={findCurrentNote()} onChange={updateNote} />
-      </Split>
+      <Header session={session} logout={() => supabase.auth.signOut()} />
+      {session ? (
+        <Split
+          className="split"
+          sizes={[25, 75]}
+          minSize={100}
+          expandToMin={false}
+          gutterSize={10}
+          gutterAlign="center"
+          snapOffset={30}
+          dragInterval={1}
+          direction="horizontal"
+          cursor="col-resize"
+        >
+          <List
+            selectedNoteId={selectedNoteId}
+            addNote={addNote}
+            deleteNote={deleteNote}
+            selectNote={selectNote}
+            notes={notes}
+          />
+          <Detail selectedNote={findCurrentNote()} onChange={updateNote} />
+        </Split>
+      ) : (
+        <Login />
+      )}
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,25 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faNoteSticky } from "@fortawesome/free-solid-svg-icons";
+import { faNoteSticky, faUser } from "@fortawesome/free-solid-svg-icons";
 
-export default function Header() {
+export default function Header(props) {
   return (
     <header className="header">
-      <FontAwesomeIcon icon={faNoteSticky} className="header--logo" />
-      <h3 className="header--title">Renote</h3>
+      <div className="header--branding">
+        <FontAwesomeIcon icon={faNoteSticky} className="header--logo" />
+        <h3 className="header--title">Renote</h3>
+      </div>
+      {props.session && props.session.user ? (
+        <div className="header--user">
+          <span className="header--logout" onClick={props.logout}>
+            Sign Out
+          </span>
+          <span className="separator"></span>
+          <FontAwesomeIcon icon={faUser} className="header--user-icon" />
+          <span>{props.session.user.email}</span>
+        </div>
+      ) : (
+        ""
+      )}
     </header>
   );
 }
